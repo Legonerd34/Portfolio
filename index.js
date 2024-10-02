1a19f4a2fc5520c19cea302c7108e3ac9d056efd
Legonerd34 <45531020+Legonerd34@users.noreply.github.com>
2024-10-02 10:39:04 +0100

    Reload function added

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const button = document.getElementById("load");
 
-const HTML = `<object type="text/html" data="fboop2/build/web/index.html" id="web">
+const HTML = `<object type="text/html" data="fboop2/build/web/index.html" id="flappy">
               </object>`;
 
 function load() {
     button.insertAdjacentHTML("beforebegin", HTML);
     console.log("loaded");
+
+    button.textContent = "Re-load";
+
+    const nodeMap = button.attributes;
+
+    nodeMap.removeNamedItem("onclick");
+    nodeMap.setNamedItem("onclick", "reload()");
+}
+
+function reload() {
+    var objects = document.getElementById("flappy");
+
+    for(var i = 0; i < objects.length; i++) {
+
+        var obj = objects[i];
+
+        obj.data = obj.data;
+    }
 }
