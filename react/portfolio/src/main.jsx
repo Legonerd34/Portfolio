fab06e261f6fae33431d98407878ac06f5f8f49d
Legonerd34 <45531020+Legonerd34@users.noreply.github.com>
2024-11-26 14:38:57 +0000

    React app added

---
+++ react/portfolio/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
