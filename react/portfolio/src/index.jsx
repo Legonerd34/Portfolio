7cd3d8b3d70f951751b81ee3c3ebea79be182f68
Legonerd34 <45531020+Legonerd34@users.noreply.github.com>
2024-11-26 16:22:00 +0000

    rebuild

--- react/portfolio/src/index.jsx
+++ react/portfolio/src/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
-
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
