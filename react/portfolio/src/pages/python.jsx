090302f1255db0bb4eb94c86d11e3ec185467c78
Legonerd34 <45531020+Legonerd34@users.noreply.github.com>
2024-11-27 12:46:38 +0000

    rebuild

--- react/portfolio/src/pages/python.jsx
+++ react/portfolio/src/pages/python.jsx
@@ -311,7 +311,7 @@ function Python(){
                     {isLoaded && (
                         <object
                             type="text/html"
-                            data="Portfolio/react/portfolio/public/fboop2/build/web/index.html"
+                            data="https://legonerd34.github.io/Portfolio/fboop2/build/web/index.html"
                             id="flappy"
                         />
                     )}
