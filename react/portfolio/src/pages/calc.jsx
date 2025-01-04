184569f67ac72ddc8a499dd0a04ff7b557d0ea43
Legonerd34 <45531020+Legonerd34@users.noreply.github.com>
2025-01-04 16:46:55 +0000

    rebuild

--- react/portfolio/src/pages/calc.jsx
+++ react/portfolio/src/pages/calc.jsx
@@ -27,8 +27,12 @@ function Calc() {
           Discord Apps
         </Typography>
         <Typography level="body-lg" sx={{ color: "lightgray" }}>
-          <img src="https://legonerd34.github.io/Portfolio/Screenshot 2025-01-03 at 13.35.48.png"/>
+          I use discord alot in my personal time and I thought it would be intersting to try and create some of my own discord apps.
+           I created this ChatGPT app which enables ChatGPT to be used and displayed on the chat itself.
+           Any member can use it without leaving the chat. It uses JavaScript and NodeScript, more information can be found at the <a href="https://github.com/Legonerd34/ChatGPT-discord-bot">Github page.</a>
         </Typography>
+        <br/>
+        <img src="https://legonerd34.github.io/Portfolio/Screenshot 2025-01-03 at 13.35.48.png"/>
       </Box>
     </Box>
   );
